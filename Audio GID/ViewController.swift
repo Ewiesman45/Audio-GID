fc9e981e3a7cecec62922db6b693db9b36008dc8
Evan Wiesman <EvanWiesman@Evans-MacBook-Air-2.local>
2017-01-12 10:09:44 -0600

    initail commit

--- Audio GID/ViewController.swift
+++ Audio GID/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var drowDown: UIPickerView!
+ 
+    
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    
+    @IBAction func play(_ sender: AnyObject)
+    {
+        player.play()
     }
+    
+    @IBAction func pause(_ sender: AnyObject)
+    {
+        player.pause()
+    }
+    
+    @IBAction func replay(_ sender: AnyObject)
+    {
+        player.currentTime = 0
+    }
+    do
+    {
+    let audioPath = Bundle.main.path(forResource: "song" , ofType : "mp3")
+    try player = AVAudioPlayer(contentOf: NSURL(fileURLWithPath: audioPath!) as URL)
+    
+    }
+    catch
+    {
+        //ERROR
+    }
+    
+    override func viewDidLoad()
+        
+    {
+        super.viewDidLoad()
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+   }
 
